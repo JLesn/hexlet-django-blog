6a79e270ffe75b35a1ba169f7589990d2ff54aad
JLesn <lesnikova.j00@gmail.com>
2024-08-06 21:53:33 +0500

    CRUD

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -4,6 +4,8 @@ from hexlet_django_blog.article import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='articles'),
+    path('<int:id>/edit/', views.ArticleFormEditView.as_view(), name ='articles_update'),
     path('<int:id>/', views.ArticleView.as_view(), name='article_id'),
-    path('create/', views.ArticleFormCreateView.as_view(), name='articles_create')
+    path('create/', views.ArticleFormCreateView.as_view(), name='articles_create'),
+    path('<int:id>/delete/', views.ArticleFormDeleteView.as_view(), name='articles_delete'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
+from django.contrib import messages
 
 from hexlet_django_blog.article.models import Article
 from .forms import ArticleForm
@@ -29,8 +30,39 @@ class ArticleFormCreateView(View):
 
     def post(self, request, *args, **kwargs):
         form = ArticleForm(request.POST)
-        if form.is_valid(): # Если данные корректные, то сохраняем данные формы
+        if form.is_valid():
             form.save()
-            return redirect('articles') # Редирект на указанный маршрут
-        # Если данные некорректные, то возвращаем человека обратно на страницу с заполненной формой
+            messages.success(request, 'added successfully')
+            return redirect('articles')
         return render(request, 'articles/create.html', {'form': form})
+
+
+class ArticleFormEditView(View):
+
+    def get(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(instance=article)
+        return render(request, 'articles/update.html', {'form': form, 'article_id': article_id})
+
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Updated successfully')
+            return redirect('articles')
+
+        return render(request, 'articles/update.html', {'form': form, 'article_id':article_id})
+
+
+class ArticleFormDeleteView(View):
+
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        if article:
+            article.delete()
+        messages.success(request, 'Deleted Successfully')
+        return redirect('articles')
